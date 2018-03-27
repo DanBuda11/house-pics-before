7daf2305586a604f80ff6b66c01049506b5f9f96
Dan Buda <danbuda@gmail.com>
2018-03-26 22:01:22 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const entry = document.getElementById('entry');
+
+const imageStart = 2747;
+const imageEnd = 2803;
+
+let arr = [];
+
+for (var i = 2747; i < 2804; i++) {
+  arr.push(i);
+}
+
+arr.forEach(function(el) {
+  let newElement = document.createElement('img');
+  newElement.setAttribute('src', `./images/IMG_${el}.jpg`);
+  newElement.style.width = '90%';
+  newElement.style.border = '1px solid #000';
+  newElement.style.boxShadow = '5px 5px 5px #AAA';
+  newElement.style.marginBottom = '1em';
+  entry.appendChild(newElement);
+});
