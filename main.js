2f91cf55df4f541f0fb1c9e91b774b7af7f1c80a
Dan Buda <danbuda@gmail.com>
2018-03-26 22:08:27 -0500

    make responsive

--- main.js
+++ main.js
@@ -13,8 +13,9 @@ arr.forEach(function(el) {
   let newElement = document.createElement('img');
   newElement.setAttribute('src', `./images/IMG_${el}.jpg`);
   newElement.style.width = '90%';
+  newElement.style.maxWidth = '600px';
   newElement.style.border = '1px solid #000';
   newElement.style.boxShadow = '5px 5px 5px #AAA';
-  newElement.style.marginBottom = '1em';
+  newElement.style.marginBottom = '2em';
   entry.appendChild(newElement);
 });
